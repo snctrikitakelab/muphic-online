0e7a4c1fcff756ae552c53ac89be9a9c6e4ec817
Keita Okawa <keita.okawa@gmail.com>
2012-05-29 17:02:46 +0900

    test

--- ez-HTML/muphic_keita_js.js
+++ ez-HTML/muphic_keita_js.js
@@ -11,12 +11,13 @@ $(function () {
 	for(var i=0;i<2;i++){
 		array[i]=[' ',' ',' '];
 	}
-	var array2 = new Array(25);
-	for(var i=0;i<2;i++){
-		array2[i]=[' ',' ',' '];
-	}
-	var pf=0;	//ピアノフラグ	
-		
+	var pf=0;	//ピアノフラグ
+	var start_f=0;	//スタートフラグ
+	var stop_f=0;	//ストップフラグ
+	var gomi_f=0;	//ゴミフラグ
+	
+	var moveStep = 5;
+	var moveSpeed = 100;	
 	draw2();
 
 	window.onload = function(){
@@ -31,11 +32,6 @@ $(function () {
 		for(var i=0;i<=1;i++){
 			for(var j=0;j<=24;j++){
 				array[i][j]=null;
-			}
-		}
-		for(var i=0;i<=1;i++){
-			for(var j=0;j<=24;j++){
-				array2[i][j]=null;
 			}
 		}
 	}
@@ -113,9 +109,27 @@ $(function () {
 			}
 			if(mouseX>=25 && mouseX<=125){					//再生ボタンクリック
 				if(mouseY>=50 && mouseY<150){
-					start();
+					var moveSpeed = 100;
+					var imgpos = array[0][0]-20;
+//					var moveStep = 5;
+//					var imgpos1 = start(imgpos);
+//					alert(imgpos);
+					start(imgpos);
+//					setTimeout('start(imgpos)',moveSpeed);
+//					alert(imgpos);
+					
 				}
 			}
+			if(mouseX>=25 && mouseX<=125){					//停止ボタンクリック
+				if(mouseY>=150 && mouseY<250){
+					stop();
+				}
+			}
+			if(mouseX>=25 && mouseX<=125){					//ゴミ箱ボタンクリック
+				if(mouseY>=250 && mouseY<350){
+					gomi();
+				}
+			}			
 			
 		}
 		//画像複製関数
@@ -128,16 +142,82 @@ $(function () {
 			}
 		}
 		
-		function start(){
-//			alert("kiteru");
-			for(var time=0;time<=(array[0][0]-20);time++){
-				for(var i=0;i<100000;i++){}
-				var x = array[0][0];
-				var pos = (x-20)-time;	
-				var onpu = new Image();
-				onpu.src = "gazou/onpu.gif";
-				context.drawImage(onpu,pos,array[1][0]-25);
+		//再生処理
+		function start(imgpos){
+//			alert("start");
+//			for(var time=0;time<=(array[0][0]-20);time++){
+//				for(var i=0;i<1000;i++){
+//					for(var j=0;j<1000;j++){
+//					}			
+//				}
+//				var x = array[0][0];
+//				var pos = (x-20)-time;	
+//				var onpu = new Image();
+//				onpu.src = "gazou/onpu.gif";
+//				context.drawImage(onpu,pos,array[1][0]-25);
+//			}
+//			var imgpos = array[0][0];
+//			alert("start");
+//			var moveStep = 5;
+//			var moveSpeed = 100;
+//			alert("start");
+//			document.getElementById("onpu").style.top = array[1][0];
+//			alert("start");
+//			document.getElementById("onpu").style.left = imgpos;
+//			if(150-array[0][0] > 0){
+//				imgpos += moveStep;
+//				if(imgpos > 150){
+//					imgpos = array[0][0];
+//				}	
+//			}
+//			else{
+//			alert("start");
+			while(imgpos>=150){
+//				alert(imgpos);
+				imgpos -= moveStep;
+//				var imgpos1 = imgpos;
+//				alert(imgpos);
+//				alert(imgpos1);
+				if(imgpos < 150){
+					imgpos = array[0][0];
+//					alert("2.5");
+					break;
+				}
+	//			alert("3");
+				var onpu1 = new Image();
+				onpu1.src = "gazou/onpu.gif";
+				context.drawImage(onpu1,imgpos,array[1][0]-25);
+//				alert(imgpos);
+				Sleep(1);				//1秒待つ
+			}	
+//			}
+//			document.getElementById("onpu").style.visibility="visible";
+//			setTimeout("start(imgpos)",moveSpeed);
+//			return(imgpos1);
+
+//			alert("2");
+		}
+		
+		function Sleep(T){
+//			alert("a");
+			var d1 = new Date().getTime(); 
+//			var d2 = new Date().getTime(); 
+			var d2 = d1;
+			while( d2 < d1+1000*T ){    //T秒待つ 
+				d2=new Date().getTime();
 			}
+//			alert("b");
+			return; 
+		}
+		
+		//停止処理
+		function stop(){
+			alert("stop");
+		}
+		
+		//削除処理
+		function gomi(){
+			alert("gomi");
 		}
 		
 		//音出力関数				
@@ -159,10 +239,10 @@ $(function () {
 	//画像出力関数		
 	function draw2(){
 		var start = new Image();
-		start.src = "gazou/start_button.gif";
+		start.src = "gazou/start_button.png";
 		context.drawImage(start,25,50);
 		var stop = new Image();
-		stop.src = "gazou/stop_button.gif";
+		stop.src = "gazou/stop_button.png";
 		context.drawImage(stop,25,150);
 		var gomi = new Image();
 		gomi.src = "gazou/gomibako.jpg";
