964c31bcb68931c80d61ab4c4582229a414c2dd0
MasatoNarita <s38325@sendai-nct.jp>
2012-06-13 15:12:58 +0900

    violin

--- ez-HTML/mu_js.js
+++ ez-HTML/mu_js.js
@@ -17,14 +17,18 @@ $(function () {
 		array2[i]=[' ',' ',' '];
 	}
 	
-	var pf=0;		//ピアノフラグ
+	var sflag = null;
+	var pf=0;	//ピアノフラグ
+	var gf=0;
+	var tf=0;
+	var vf=0;
 	var start_f=0;	//スタートフラグ
 	var stop_f=0;	//ストップフラグ
 	var gomi_f=0;	//ゴミフラグ
 	var onpu_f = new Array(25);		//音符フラグ
 	
 	var moveStep = 1;
-	var moveSpeed = 15;
+	var moveSpeed = 10;
 	var imgpos_x = 0;
 	var imgpos_y = 0;
 	draw2();
@@ -39,7 +43,7 @@ $(function () {
 	
 	//配列初期化
 	function reset(){
-		for(var i=0;i<=1;i++){
+		for(var i=0;i<3;i++){
 			for(var j=0;j<=24;j++){
 				array[i][j]=null;
 				array2[i][j]=null;
@@ -53,7 +57,7 @@ $(function () {
 	//譜面作成関数				
 	function draw(){
 		context.strokeStyle = "black";
-		context.fillStyle = "white";	
+		context.fillStyle = "#FFCCFF";	
 		context.beginPath();
 		context.moveTo(150,100);
 		context.lineTo(150,500);
@@ -63,13 +67,18 @@ $(function () {
 		context.fill();
 		context.stroke();
 
-		context.beginPath();
-		for(var i=1;i<=10;i++){	
+		context.fillStyle = "#CCFFFF";			
+		for(var i=1;i<=10;i+=2){	
 			var n = 40*i;
+			context.beginPath();
 			context.moveTo(150,100+n);
 			context.lineTo(1300,100+n);
+			context.lineTo(1300,140+n);
+			context.lineTo(150,140+n);
+			context.closePath();
+			context.fill();				
+			context.stroke();				
 		}
-		context.stroke();
 				
 		context.beginPath();
 		for(var i=1;i<=16;i++){	
@@ -79,11 +88,11 @@ $(function () {
 		}
 		context.stroke();
 	}
-	
+		
 	//ボタン関連関数		
 	function draw1(){
-	
-		canvas.onmousedown = mouseDownListner;		
+		canvas.onmousedown = mouseDownListner;
+		
 		//クリック検出関数		
 		function mouseDownListner(e){
 			context.clearRect(0,0,cW,cH);
@@ -95,8 +104,21 @@ $(function () {
 			context.fillText("イベント：マウスダウン", 5, 36);
 			draw();
 			if(pf==1){
-				copy("piano",e);
+				copy("piano_",e);
+				sflag = "piano_";
 			}
+			else if(gf==1){
+				copy("guitar_",e);
+				sflag = "guitar_";
+			}
+			else if(tf==1){
+				copy("tranpet_",e);
+				sflag = "tranpet_";
+			}
+			else if(vf==1){
+				copy("violin_",e);
+				sflag = "violin_";
+			}	
 			check();
 			draw2();
 			remember();
@@ -104,25 +126,72 @@ $(function () {
 		
 		//クリック位置検出関数		
 		function check(){
-			humen(mouseX,mouseY);
+			humen(mouseX,mouseY,sflag);
 			if(mouseX>=50 && mouseX<=150){					//音ボタンクリック
 				if(mouseY>=550 && mouseY<650){
 					if(pf==0){
 						pf=1;
+						gf=0;
+						tf=0;
+						vf=0;
 					}
 					else{
 						pf=0;
 					}
 				}
 			}
+			
+			if(mouseX>=150 && mouseX<=250){					//音ボタンクリック
+				if(mouseY>=550 && mouseY<650){
+					if(gf==0){
+						gf=1;
+						pf=0;
+						tf=0;
+						vf=0;
+					}
+					else{
+						gf=0;
+					}
+				}
+			}
+			
+			if(mouseX>=250 && mouseX<=350){					//音ボタンクリック
+				if(mouseY>=550 && mouseY<650){
+					if(tf==0){
+						tf=1;
+						pf=0;
+						gf=0;
+						vf=0;
+					}
+					else{
+						tf=0;
+					}
+				}
+			}
+			
+			if(mouseX>=350 && mouseX<=450){					//音ボタンクリック
+				if(mouseY>=550 && mouseY<650){
+					if(vf==0){
+						vf=1;
+						pf=0;
+						tf=0;
+						gf=0;
+					}
+					else{
+						vf=0;
+					}
+				}
+			}
+			
 			if(mouseX>=25 && mouseX<=125){					//再生ボタンクリック
 				if(mouseY>=50 && mouseY<150){
 					imgpos_x = imgget_x();
 					for(var i=0;i<25;i++){
 						array2[0][i] = array[0][i];
 						array2[1][i] = array[1][i];
+						array2[2][i] = array[2][i];
 					}
-					start();
+					start();	
 				}
 			}
 			if(mouseX>=25 && mouseX<=125){					//停止ボタンクリック
@@ -138,26 +207,42 @@ $(function () {
 		}
 		
 		//譜面クリック位置検出関数
-		function humen(mouseX,mouseY){
-			if(pf==1){
+		function humen(mouseX,mouseY,icon){
+			if(pf==1 || gf==1 || tf==1 || vf==1){
 				if(mouseX>=150 && mouseX<=1300){			//譜面クリック
-					var count=0;
+					var count=0;				
 					for(var i=500;i>=140;i-=40){
 						count++;
 						if(mouseY>=i-40 && mouseY<i){
-							output(count);
+							output(count,icon);
 						}
 					}
 				}
 			}
+			
 		}
 		
 		//画像複製関数
 		function copy(icon,e){
 			adjustXY(e);
 			if(mouseX>=150 && mouseX<=1300 && mouseY>=100 && mouseY<=500){
+				for(var i=500;i>=140;i-=40){
+					if(mouseY>=i-40 && mouseY<i){
+						mouseY = i-20;
+						break;
+					}
+				}
+				
+				for(var i=150;i<=1300;i+=71.875){
+					if(mouseX>=i && mouseX<i+71.875){
+						mouseX = i+35.9375;
+						break;
+					}
+				}
+
 				array[0][k]=mouseX-20;
 				array[1][k]=mouseY-25;
+				array[2][k]=icon;
 				k++;
 			}
 		}
@@ -178,7 +263,7 @@ $(function () {
 				}	
 			}
 			return imgpos_x;
-		}	
+		}
 		
 		//背景出力関数
 		function outhaikei(){
@@ -207,7 +292,8 @@ $(function () {
 				}
 			}
 			return arraylast;
-		}
+		}		
+		
 		
 		//再生処理
 		function start(){
@@ -221,15 +307,15 @@ $(function () {
 				for(var i=0;i<25;i++){
 					if(array2[0][i]>0){
 						if(array2[0][i] <= 130){
-							humen(array2[0][i]+20,array2[1][i]+25);			//音符画像の真ん中で音を出すためにxのみ+20
+							humen(array2[0][i]+55.9375,array2[1][i]+25,array2[2][i]);	
 							onpu_f[i] = 1;				//音が鳴り次第その音符画像は描写の必要がなくなる
 						}
 						outhaikei();					//背景の描写
-						for(var j=0;j<=arraylast;j++){
+						for(var j=0;j<arraylast;j++){
 							if(onpu_f[j] == 0){			//描写が必要な音符画像だけ
 								outonpu(array2[0][j],array2[1][j]);			//音符画像の描写
 							}
-						}
+						}						
 					}
 				}
 				if(imgpos_x > 130){
@@ -256,14 +342,10 @@ $(function () {
 		}
 		
 		//音出力関数				
-		function output(e){
-			if(e>=9){
-				alert("音源ない(´・ω・｀)");
-				draw2();
-			}
-			else	document.getElementById("piano_"+e).play();
-		}
-	
+		function output(e,icon){
+			document.getElementById(icon+e).play();
+		}	
+			
 		//クリック座標検出関数					
 		function adjustXY(e){
 			var rect = e.target.getBoundingClientRect();
@@ -275,13 +357,13 @@ $(function () {
 	//画像出力関数		
 	function draw2(){
 		var start = new Image();
-		start.src = "gazou/start_button.png";
+		start.src = "gazou/start_button.gif";
 		context.drawImage(start,25,50);
 		var stop = new Image();
-		stop.src = "gazou/stop_button.png";
+		stop.src = "gazou/stop_button.gif";
 		context.drawImage(stop,25,150);
 		var gomi = new Image();
-		gomi.src = "gazou/gomibako.jpg";
+		gomi.src = "gazou/keshigomu_1.gif";
 		context.drawImage(gomi,25,250);
 		
 		var piano = new Image();
@@ -292,11 +374,38 @@ $(function () {
 			piano.src = "gazou/piano_1.gif";
 		}
 		context.drawImage(piano,50,550);
+		
+		var guitar = new Image();
+		if(gf==1){
+			guitar.src = "gazou/guitar_2.gif";
+		}
+		else{
+			guitar.src = "gazou/guitar_1.gif";
+		}
+		context.drawImage(guitar,150,550);
+
+		var tranpet = new Image();
+		if(tf==1){
+			tranpet.src = "gazou/tranpet_2.gif";
+		}
+		else{
+			tranpet.src = "gazou/tranpet_1.gif";
+		}
+		context.drawImage(tranpet,250,550);
+		
+		var violin = new Image();
+		if(vf==1){
+			violin.src = "gazou/violin_2.gif";
+		}
+		else{
+			violin.src = "gazou/violin_1.gif";
+		}
+		context.drawImage(violin,350,550);
 	}
 	
 	//音符画像初期位置記憶関数
-	function remember(){
-		for(var i=0;i<25;i++){
+	function remember(){				
+		for(var i=0;i<=24;i++){
 			if(array[0][i] != null){
 				var onpu = new Image();
 				onpu.src = "gazou/onpu.gif";
