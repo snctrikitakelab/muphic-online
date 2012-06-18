2a6704e772a222940d168f4294215cec02bd8377
Keita Okawa <keita.okawa@gmail.com>
2012-06-18 18:39:11 +0900

    owaranaina

--- ez-HTML/muphic_keita_js.js
+++ ez-HTML/muphic_keita_js.js
@@ -9,9 +9,9 @@ $(function () {
 	var k=0;
 	var array = new Array(25);			//音符位置データ
 	var array2 = new Array(25);
-	for(var i=0;i<3;i++){
-		array[i]=[' ',' ',' '];
-		array2[i]=[' ',' ',' '];
+	for(var i=0;i<4;i++){
+		array[i]=[' ',' ',' ',' '];
+		array2[i]=[' ',' ',' ',' '];
 	}
 
 	var sflag = null;
@@ -21,8 +21,8 @@ $(function () {
 	var vf=0;
 	var start_f=0;	//スタートフラグ
 	var stop_f=0;	//ストップフラグ
-	var gomi_f=0;	//ゴミフラグ
-	var onpu_f = new Array(25);		//音符フラグ
+	var eraser_f=0;	//消しゴムフラグ
+//	var gomi_f=0;	//ゴミフラグ
 	var arraylast=0;	
 	
 	var moveStep = 2;
@@ -41,11 +41,11 @@ $(function () {
 	
 	//配列初期化
 	function reset(){
-		for(var i=0;i<3;i++){
+		for(var i=0;i<4;i++){
 			for(var j=0;j<=24;j++){
 				array[i][j]=null;
+				array[3][j]=0;
 				array2[i][j]=null;
-				onpu_f[j]=0;
 			}
 		}
 	}
@@ -119,59 +119,53 @@ $(function () {
 		
 		//クリック位置検出関数		
 		function check(){
-			humen(mouseX,mouseY,sflag);
-			if(mouseX>=50 && mouseX<=150){					//音ボタンクリック
-				if(mouseY>=550 && mouseY<650){
-					if(pf==0){
-						pf=1;
-						gf=0;
-						tf=0;
-						vf=0;
-					}
-					else{
-						pf=0;
-					}
-				}
-			}
-			
-			if(mouseX>=150 && mouseX<=250){					//音ボタンクリック
-				if(mouseY>=550 && mouseY<650){
-					if(gf==0){
-						gf=1;
-						pf=0;
-						tf=0;
-						vf=0;
-					}
-					else{
-						gf=0;
+			humen(mouseX,mouseY,sflag);						//譜面クリック
+			if(eraser_f==0){
+				if(mouseX>=50 && mouseX<=150){					//音ボタンクリック
+					if(mouseY>=550 && mouseY<650){
+						if(pf==0){
+							pf=1;
+							gf=tf=vf=0;
+						}
+						else{
+							pf=0;
+						}
 					}
 				}
-			}
 			
-			if(mouseX>=250 && mouseX<=350){					//音ボタンクリック
-				if(mouseY>=550 && mouseY<650){
-					if(tf==0){
-						tf=1;
-						pf=0;
-						gf=0;
-						vf=0;
-					}
-					else{
-						tf=0;
+				if(mouseX>=150 && mouseX<=250){					//音ボタンクリック
+					if(mouseY>=550 && mouseY<650){
+						if(gf==0){
+							gf=1;
+							pf=tf=vf=0;
+						}
+						else{
+							gf=0;
+						}
 					}
 				}
-			}
 			
-			if(mouseX>=350 && mouseX<=450){					//音ボタンクリック
-				if(mouseY>=550 && mouseY<650){
-					if(vf==0){
-						vf=1;
-						pf=0;
-						tf=0;
-						gf=0;
+				if(mouseX>=250 && mouseX<=350){					//音ボタンクリック
+					if(mouseY>=550 && mouseY<650){
+						if(tf==0){
+							tf=1;
+							pf=gf=vf=0;
+						}
+						else{
+							tf=0;
+						}
 					}
-					else{
-						vf=0;
+				}
+				
+				if(mouseX>=350 && mouseX<=450){					//音ボタンクリック
+					if(mouseY>=550 && mouseY<650){
+						if(vf==0){
+							vf=1;
+							pf=tf=gf=0;
+						}
+						else{
+							vf=0;
+						}
 					}
 				}
 			}
@@ -183,32 +177,48 @@ $(function () {
 						array2[0][i] = array[0][i];
 						array2[1][i] = array[1][i];
 						array2[2][i] = array[2][i];
+						array2[3][i] = array[3][i];						
 					}
-					arraylast = getlast(arraylast);
-					start();	
+					arraylast = getlast();
+					start();
+					eraser_f=0;	
 				}
 			}
 			if(mouseX>=25 && mouseX<=125){					//停止ボタンクリック
 				if(mouseY>=150 && mouseY<250){
 					stop();
+					eraser_f=0;
 				}
 			}
-			if(mouseX>=25 && mouseX<=125){					//ゴミ箱ボタンクリック
+			if(mouseX>=25 && mouseX<=125){					//消しゴムボタンクリック
 				if(mouseY>=250 && mouseY<350){
-					gomi();
+//					arraylast = getlast();			
+					if(eraser_f==0){
+						eraser_f=1;
+						pf=gf=tf=vf=0;
+					}
+					else{
+						eraser_f=0;
+					}
+					eraser();
 				}
 			}
 		}
 		
 		//譜面クリック位置検出関数
 		function humen(mouseX,mouseY,icon){
-			if(pf==1 || gf==1 || tf==1 || vf==1){
-				if(mouseX>=150 && mouseX<=1300){			//譜面クリック
-					var count=0;				
+			if(pf==1 || gf==1 || tf==1 || vf==1 || arraylast>=1){
+				if(mouseX>=150 && mouseX<=1300){
+					var count=0;
 					for(var i=500;i>=140;i-=40){
 						count++;
 						if(mouseY>=i-40 && mouseY<i){
-							output(count,icon);
+							if(eraser_f==1){
+								del(mouseX,mouseY);
+							}
+							else{
+								output(count,icon);
+							}
 						}
 					}
 				}
@@ -236,6 +246,7 @@ $(function () {
 				array[0][k]=mouseX-20;
 				array[1][k]=mouseY-25;
 				array[2][k]=icon;
+				array[3][k]=1;
 				k++;
 			}
 		}
@@ -275,7 +286,7 @@ $(function () {
 		}
 		
 		//音符画像個数調査関数
-		function getlast(arraylast){	
+		function getlast(){	
 			for(var i=0;i<25;i++){
 				if(array2[1][i] != null){
 					arraylast++;
@@ -294,11 +305,11 @@ $(function () {
 					if(array2[0][i]>125){
 						if(array2[0][i] <= 130){
 							humen(array2[0][i]+55.9375,array2[1][i]+25,array2[2][i]);	
-							onpu_f[i] = 1;				//音が鳴り次第その音符画像は描写の必要がなくなる
+							array[3][i] = 0;				
 						}
 						outhaikei();					//背景の描写
 						for(var j=0;j<arraylast;j++){
-							if(onpu_f[j] == 0){			//描写が必要な音符画像だけ
+							if(array[3][j] == 1){			//描写が必要な音符画像だけ
 								outonpu(array2[0][j],array2[1][j]);			//音符画像の描写
 							}
 						}						
@@ -316,15 +327,38 @@ $(function () {
 		//停止処理
 		function stop(){
 			for(var i=0;i<=24;i++){
-				onpu_f[i]=0;
+				array[3][i]=1;
 			}
 			imgpos_x = 0;
 			remember();
 		}
 		
 		//削除処理
-		function gomi(){
-			alert("gomi");
+		function eraser(){
+//			alert("消しゴム");
+			stop();
+//			if(eraser_f==1){
+//				del();
+//			}
+		}
+		
+		//音符削除関数
+		function del(mouseX,mouseY){
+//			alert("kesuyo");
+//			arraylast = getlast();
+//			for(var i=0;i<arraylast;i++){
+//			}
+//			adjustXY(e);
+			alert(mouseX);
+			for(var i=500;i>=140;i-=40){
+				
+			}
+//			alert(mouseY);
+			alert(array[0][0]);
+			alert(array2[0][0]);
+//			alert(array[1][0]);
+//			alert(array[0][1]);
+//			alert(array[1][1]);		
 		}
 		
 		//音出力関数				
@@ -349,7 +383,13 @@ $(function () {
 		stop.src = "gazou/stop_button.gif";
 		context.drawImage(stop,25,150);
 		var gomi = new Image();
-		gomi.src = "gazou/keshigomu_1.gif";
+		if(eraser_f==1){
+			gomi.src = "gazou/keshigomu_2.gif";
+		}
+		else{
+			gomi.src = "gazou/keshigomu_1.gif";
+		}
+		
 		context.drawImage(gomi,25,250);
 		
 		var piano = new Image();
